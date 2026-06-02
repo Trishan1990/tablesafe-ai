e79c957e6f5cdef03399e2a2a4b15fcec8da3fbc
Trishan1990 <64273061+Trishan1990@users.noreply.github.com>
2026-06-02 11:25:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import json
 from pathlib import Path
 import plotly.express as px
+import plotly.graph_objects as go
 
 st.set_page_config(
     page_title="TableSafe AI | EXP-006",
